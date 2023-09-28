7527a2ec5a423950c8a232e6f52b9376314016fe
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2023-09-28 09:45:15 +0300

    Update pxt.json, main.blocks, main.ts, RGB-WS2812B.ts, Gesture.ts, Others.ts, MotorDriver.ts, TouchSensor.ts, Esp01.ts, IR.ts, TempAndHum.ts, Oled.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,9 @@
-//% icon="\uf26c"
-//% color="255" weight="90"
 namespace PicoBricks {
-    //% block="test123"
-    //% subcategory="OTHERS"
-    export function test123(): number {
-        let result = pins.analogReadPin(AnalogPin.P0);
-        return result;
-    }
 
-    //% block="Pot read"
-    //% subcategory="OTHERS"
-    export function potread(): number {
-        let result2 = pins.analogReadPin(AnalogPin.P1);
-        return result2;
-    }
+
 }
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    PicoBricks.init(128, 64)
+    PicoBricks.writeStringNewLine("merhaba")
+    basic.pause(1000)
 })
