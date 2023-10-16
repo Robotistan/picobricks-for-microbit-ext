608d4b7281116775da1453941838afa53426750f
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2023-10-16 09:47:35 +0300

    Update pxt.json, main.blocks, Others.ts

--- Others.ts
+++ Others.ts
@@ -5,6 +5,8 @@ enum set_relay {
 
 namespace PicoBricks {
     let result = 0;
+    let trigpin = 0;
+    let echopin = 0;
 
     //% block="Light Sensor Read"
     //% subcategory="Others"
@@ -39,6 +41,28 @@ namespace PicoBricks {
     export function pirread(): number {
         result = pins.digitalReadPin(DigitalPin.P13);
         return result;
+    }
+
+    //% block="Ultrasonic Distance Sensor With Trig Pin At %pin1 And Echo Pin At %pin2"
+    //% subcategory="Others"
+    export function hcsr_init(pin1: DigitalPin, pin2: DigitalPin): void {
+        trigpin = pin1
+        echopin = pin2
+    }
+
+    //% block="Read Ultrasonic Distance"
+    //% subcategory="Others"
+    export function hcsr_read(pin1: DigitalPin, pin2: DigitalPin): number {
+        pins.digitalWritePin(trigpin, 0)
+        control.waitMicros(5);
+        pins.digitalWritePin(trigpin, 1)
+        control.waitMicros(10);
+        pins.digitalWritePin(trigpin, 0)
+
+        let duration = pins.pulseIn(echopin, 1)
+        let cm = (duration / 2) / 29.1
+
+        return cm
     }
     
 }
