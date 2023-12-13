086d264900ea10502188eb890e674446597d142d
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2023-12-13 10:32:28 +0300

    Update IR.ts

--- IR.ts
+++ IR.ts
@@ -193,6 +193,7 @@ namespace picobricks {
      * @param pin describe parameter here, eg: DigitalPin.P15
      */
     //% subcategory="IR Receiver"
+    //% blockId=connectIrReceiver
     //% block="connect IR receiver at pin %pin"
     //% weight=90
     export function connectIrReceiver(pin: DigitalPin): void {
@@ -221,7 +222,7 @@ namespace picobricks {
      * When the selected IR controller button is pressed
      */
     //% subcategory="IR Receiver"
-    //% blockId=irButton
+    //% blockId=onIrButton
     //% block="on IR button | %button | %action"
     //% button.fieldEditor="gridpicker"
     //% button.fieldOptions.columns=3
