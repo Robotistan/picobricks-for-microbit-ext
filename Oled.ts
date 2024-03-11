a8cd0e35babde0716c78c65d84a5d0ad4636874b
M. Suat Morkan <79283872+MSMorkan-Robo@users.noreply.github.com>
2024-03-11 11:14:17 +0300

    Update Oled.ts
    Licence update

--- Oled.ts
+++ Oled.ts
@@ -1,5 +1,6 @@
 //The MIT License (MIT)
-//Copyright (c) <2024> <https://github.com/makecode-extensions/OLED12864_I2C>
+//Copyright (c) <2018> microbit/micropython Chinese community
+//<https://github.com/makecode-extensions/OLED12864_I2C>
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -390,7 +391,7 @@ namespace picobricks {
      * OLED initialize
      * @param addr is i2c addr, eg: 60
      */
-    //% blockId=oledinit block="init oled with addr %addr"
+    //% blockId=oledinit block="init OLED with addr %addr"
     //% weight=100 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
     //% subcategory="OLED"
