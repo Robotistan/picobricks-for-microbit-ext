0e9b315ada2b4a500a43e55a203e2dbf0bf3cdec
M. Suat Morkan <79283872+MSMorkan-Robo@users.noreply.github.com>
2024-11-12 10:08:37 +0300

    Update MotorDriver.ts

--- MotorDriver.ts
+++ MotorDriver.ts
@@ -1,18 +1,18 @@
-enum dcMotorType {
-    dc1 = 1,
-    dc2 = 2
+enum PicoBricksDcMotorType {
+    Dc1 = 1,
+    Dc2 = 2
 }
 
-enum servoMotorType {
-    servo1 = 3,
-    servo2 = 4,
-    servo3 = 5,
-    servo4 = 6
+enum PicoBricksServoMotorType {
+    Servo1 = 3,
+    Servo2 = 4,
+    Servo3 = 5,
+    Servo4 = 6
 }
 
-enum directionType {
-    forward = 0,
-    backward = 1
+enum PicoBricksDirectionType {
+    Forward = 0,
+    Backward = 1
 }
 
 //% weight=10 color=#067565 block="PicoBricks" icon="\uf135"
@@ -23,10 +23,10 @@ namespace picobricks {
      * It brings the selected servo motor to the set angle
      */
     //% blockId=servomotor 
-    //% block="servo motor %servoMotorType and angle %angle"
+    //% block="servo motor %PicoBricksServoMotorType and angle %angle"
     //% angle.min=0 angle.max=180
     //% subcategory="Motor Driver"
-    export function servomotor(Servo_type: servoMotorType, angle: number): void {
+    export function servomotor(Servo_type: PicoBricksServoMotorType, angle: number): void {
         pins.i2cWriteNumber(MOTOR_DRIVER_ADDRESS, 0x26, NumberFormat.UInt8BE, false)
         pins.i2cWriteNumber(MOTOR_DRIVER_ADDRESS, Servo_type, NumberFormat.UInt8BE, false)
         pins.i2cWriteNumber(MOTOR_DRIVER_ADDRESS, 0x00, NumberFormat.UInt8BE, false)
@@ -39,10 +39,10 @@ namespace picobricks {
      * It runs the selected DC motor forward or reverse at the set speed and direction
      */
     //% blockId=dcmotor
-    //% block="dc motor %dcMotorType and speed %speed and direction %direction"
+    //% block="dc motor %PicoBricksDcMotorType and speed %speed and direction %direction"
     //% speed.min=0 speed.max=100
     //% subcategory="Motor Driver"
-    export function dcmotor(dc_type: dcMotorType, speed: number, direction: directionType): void {
+    export function dcmotor(dc_type: PicoBricksDcMotorType, speed: number, direction: PicoBricksDirectionType): void {
         let mspeed = Math.map(speed,0,100,0,255)
         pins.i2cWriteNumber(MOTOR_DRIVER_ADDRESS, 0x26, NumberFormat.UInt8BE, false)
         pins.i2cWriteNumber(MOTOR_DRIVER_ADDRESS, dc_type, NumberFormat.UInt8BE, false)
