9f852bd57c418c88bed76b6f976c45e12b850a97
M. Suat Morkan <79283872+MSMorkan-Robo@users.noreply.github.com>
2024-11-14 18:21:14 +0300

    Update Oled.ts

--- Oled.ts
+++ Oled.ts
@@ -354,12 +354,13 @@ namespace picobricks {
         let line = pins.createBuffer(s.length+1);
         for (let n = 0; n < s.length; n++) {
             for (let i = 0; i < 6; i++) {
-            if (i === 5) {
-                line[1] = 0x00
-            } else {
-                let charIndex = c.charCodeAt(n)
-                p = font.getNumber(NumberFormat.UInt8BE, 5 * charIndex + i)
-                line[n+1] = p
+                if (i === 5) {
+                    line[n+1] = 0x00
+                } else {
+                    let charIndex = c.charCodeAt(n)
+                    p = font.getNumber(NumberFormat.UInt8BE, 5 * charIndex + i)
+                    line[n+1] = p
+                }
             }
         }
         set_pos(x * 5, y)
