7191e1c7804fcee32ed22b872612ee949ab38d0d
M. Suat Morkan <79283872+MSMorkan-Robo@users.noreply.github.com>
2024-11-15 17:13:32 +0300

    Update Gesture.ts

--- Gesture.ts
+++ Gesture.ts
@@ -25,18 +25,9 @@ enum PicoBricksGestureType {
 
 namespace picobricks {
     const ADDR = 0x39
-    const APDS9960_RAM = 0x00
     const APDS9960_ENABLE = 0x80
     const APDS9960_ATIME = 0x81
     const APDS9960_WTIME = 0x83
-    const APDS9960_AILTIL = 0x84
-    const APDS9960_AILTH = 0x85
-    const APDS9960_AIHTL = 0x86
-    const APDS9960_AIHTH = 0x87
-    const APDS9960_PILT = 0x89
-    const APDS9960_PIHT = 0x8B
-    const APDS9960_PERS = 0x8C
-    const APDS9960_CONFIG1 = 0x8D
     const APDS9960_PPULSE = 0x8E
     const APDS9960_CONTROL = 0x8F
     const APDS9960_CONFIG2 = 0x90
@@ -51,10 +42,6 @@ namespace picobricks {
     const APDS9960_BDATAL = 0x9A
     const APDS9960_BDATAH = 0x9B
     const APDS9960_PDATA = 0x9C
-    const APDS9960_POFFSET_UR = 0x9D
-    const APDS9960_POFFSET_DL = 0x9E
-    const APDS9960_CONFIG3 = 0x9F
-    const APDS9960_GPENTH = 0xA0
     const APDS9960_GEXTH = 0xA1
     const APDS9960_GCONF1 = 0xA2
     const APDS9960_GCONF2 = 0xA3
@@ -66,10 +53,6 @@ namespace picobricks {
     const APDS9960_GCONF3 = 0xAA
     const APDS9960_GCONF4 = 0xAB
     const APDS9960_GFLVL = 0xAE
-    const APDS9960_GSTATUS = 0xAF
-    const APDS9960_IFORCE = 0xE4
-    const APDS9960_PICLEAR = 0xE5
-    const APDS9960_CICLEAR = 0xE6
     const APDS9960_AICLEAR = 0xE7
     const APDS9960_GFIFO_U = 0xFC
     const APDS9960_GFIFO_D = 0xFD
