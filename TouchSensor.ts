436439e54e561ee25245bd55ec7ca9b482b42d0d
M. Suat Morkan <79283872+MSMorkan-Robo@users.noreply.github.com>
2024-11-15 17:54:12 +0300

    Update TouchSensor.ts

--- TouchSensor.ts
+++ TouchSensor.ts
@@ -62,105 +62,22 @@ namespace picobricks {
     const PROX_STAT = 0xAE;
     const CTRL_CMD = 0x86;
     const BUTTON_STATUS	= 0xAA;
-    const SAVE_CHECK_CRC = 0x02;
-    const SW_RESET = 0xFF;
-
-    const CY8CMBR3xxx_CONFIG_DATA_LENGTH = 126;
-    const CY8CMBR3xxx_CRC_BIT4_MASK = 0x0F;
-    const CY8CMBR3xxx_CRC_BIT4_SHIFT = 0x04;
-    const CY8CMBR3xxx_CCITT16_DEFAULT_SEED = 0xffff;
-    const CY8CMBR3xxx_CCITT16_POLYNOM = 0x1021;
-    const CY8CMBR3xxx_CRC_BIT_WIDTH = 32;
-
-    const NOTE_B0 = 31
-    const NOTE_C1 = 33
-    const NOTE_CS1 = 35
-    const NOTE_D1 = 37
-    const NOTE_DS1 = 39
-    const NOTE_E1 = 41
-    const NOTE_F1 = 44
-    const NOTE_FS1 = 46
-    const NOTE_G1 = 49
-    const NOTE_GS1 = 52
-    const NOTE_A1 = 55
-    const NOTE_AS1 = 58
-    const NOTE_B1 = 62
-    const NOTE_C2 = 65
-    const NOTE_CS2 = 69
-    const NOTE_D2 = 73
-    const NOTE_DS2 = 78
-    const NOTE_E2 = 82
-    const NOTE_F2 = 87
-    const NOTE_FS2 = 93
-    const NOTE_G2 = 98
-    const NOTE_GS2 = 104
-    const NOTE_A2 = 110
-    const NOTE_AS2 = 117
-    const NOTE_B2 = 123
-    const NOTE_C3 = 131
-    const NOTE_CS3 = 139
-    const NOTE_D3 = 147
-    const NOTE_DS3 = 156
-    const NOTE_E3 = 165
-    const NOTE_F3 = 175
-    const NOTE_FS3 = 185
-    const NOTE_G3 = 196
-    const NOTE_GS3 = 208
-    const NOTE_A3 = 220
-    const NOTE_AS3 = 233
-    const NOTE_B3 = 247
+    
     const NOTE_C4 = 262
-    const NOTE_CS4 = 277
     const NOTE_D4 = 294
-    const NOTE_DS4 = 311
     const NOTE_E4 = 330
     const NOTE_F4 = 349
-    const NOTE_FS4 = 370
     const NOTE_G4 = 392
-    const NOTE_GS4 = 415
     const NOTE_A4 = 440
-    const NOTE_AS4 = 466
     const NOTE_B4 = 494
     const NOTE_C5 = 523
-    const NOTE_CS5 = 554
     const NOTE_D5 = 587
-    const NOTE_DS5 = 622
     const NOTE_E5 = 659
     const NOTE_F5 = 698
-    const NOTE_FS5 = 740
     const NOTE_G5 = 784
-    const NOTE_GS5 = 831
     const NOTE_A5 = 880
-    const NOTE_AS5 = 932
     const NOTE_B5 = 988
     const NOTE_C6 = 1047
-    const NOTE_CS6 = 1109
-    const NOTE_D6 = 1175
-    const NOTE_DS6 = 1245
-    const NOTE_E6 = 1319
-    const NOTE_F6 = 1397
-    const NOTE_FS6 = 1480
-    const NOTE_G6 = 1568
-    const NOTE_GS6 = 1661
-    const NOTE_A6 = 1760
-    const NOTE_AS6 = 1865
-    const NOTE_B6 = 1976
-    const NOTE_C7 = 2093
-    const NOTE_CS7 = 2217
-    const NOTE_D7 = 2349
-    const NOTE_DS7 = 2489
-    const NOTE_E7 = 2637
-    const NOTE_F7 = 2794
-    const NOTE_FS7 = 2960
-    const NOTE_G7 = 3136
-    const NOTE_GS7 = 3322
-    const NOTE_A7 = 3520
-    const NOTE_AS7 = 3729
-    const NOTE_B7 = 3951
-    const NOTE_C8 = 4186
-    const NOTE_CS8 = 4435
-    const NOTE_D8 = 4699
-    const NOTE_DS8 = 4978
 
     let rec_buf = pins.createBuffer(3);
     let buff = pins.createBuffer(32);
@@ -366,12 +283,12 @@ namespace picobricks {
         pins.i2cWriteBuffer(CHIP_ADDRESS, buff5, false)
         ///////////////////////////////////////////////
         buff2[0] = CTRL_CMD
-        buff2[1] = SAVE_CHECK_CRC
+        buff2[1] = 0x02 //SAVE_CHECK_CRC
         pins.i2cWriteBuffer(CHIP_ADDRESS, buff2, false)
         control.waitMicros(200000);
         ///////////////////////////////////////////////
         buff2[0] = CTRL_CMD
-        buff2[1] = SW_RESET
+        buff2[1] = 0xFF //SW_RESET
         pins.i2cWriteBuffer(CHIP_ADDRESS, buff2, false)
         control.waitMicros(200000);
     }
